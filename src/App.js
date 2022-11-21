40f5ee0a0ee1becac6ed3e5eb73b320c35d69707
Faaiz Akhtar <faaizakhtar@gmail.com>
2022-11-21 12:47:35 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from './components/directory/directory.component';
 
-function App() {
+
+const App = () => {
+  const categories = [
+    {
+      id:1,
+      title: 'Aari Work', 
+      imageUrl: 'https://i.ibb.co/tmnNRcd/aari.jpg',
+    },
+    {
+      id:2,
+      title: 'Zardozi Work', 
+      imageUrl: 'https://i.ibb.co/6tDd7YJ/zardozi.jpg',
+    },
+    {
+      id:3,
+      title: 'Persian Embroidery',
+      imageUrl: 'https://i.ibb.co/ctDsFg9/persian.jpg', 
+    },
+    {
+      id:4,
+      title: 'Parsi Embroidery',
+      imageUrl: 'https://i.ibb.co/bssdGJc/parsi.jpg', 
+    },
+    {
+      id:5,
+      title: 'Farisha Embroidery',
+      imageUrl: 'https://i.ibb.co/vqBXc6L/farisha.jpg', 
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories={categories} />
   );
-}
+};
 
 export default App;

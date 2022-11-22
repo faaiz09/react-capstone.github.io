31c8c24db6bb8f7291d762050ccad1d6512a2a51
faaiz09 <faaizakhtar@gmail.com>
2022-11-22 14:23:17 +0530

    Login at Navigation Bar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+import { ReactComponent as AELogo } from '../../assets/logo-AE.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <AELogo className='logo'/>
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP 
+                </Link>
+                <Link className='nav-link' to='/sign-in'>
+                    SIGN IN 
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+      </Fragment>  
+    );
+};
+
+export default Navigation;
